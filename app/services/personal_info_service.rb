0062a5a56d1b8023822e299572799f407cbf92c3
Steve Pentler <stevepentler@gmail.com>
2016-05-09 17:45:49 -0600

    Renames to personal_info_service to accomodate user info and devices

--- app/services/personal_info_service.rb
+++ app/services/personal_info_service.rb
@@ -1,4 +1,4 @@
-class UserIdService
+class PersonalInfoService
   attr_reader :client
 
   def initialize
--- test/services/personal_info_service_test.rb
+++ test/services/personal_info_service_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class UserIdServiceTest < ActiveSupport::TestCase
+class PersonalInfoServiceTest < ActiveSupport::TestCase
   test 'retrieves user id' do
-    service = UserIdService.new
+    service = PersonalInfoService.new
     assert_equal ENV['USER_ID'], service.retrieve_id
   end
 end
