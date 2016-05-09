641a772aa148fbc37a5997460d585a890ccbd499
Steve Pentler <stevepentler@gmail.com>
2016-05-09 17:10:40 -0600

    Structures Faraday call, not yet tested

--- app/services/user_id_service.rb
+++ app/services/user_id_service.rb
@@ -1,5 +1,15 @@
 class UserId
   def intialize
+    @client = Faraday.new("https://api.rach.io/1/public/person/") do |faraday|
+      faraday.headers = rachio_headers
+      faraday.adapter = Faraday.default_adapter
+    end
+  end
 
+  private
+
+  def rachio_headers
+    {"Authorization" => "Bearer #{ENV['ACCESS_TOKEN']}",
+     "Content-Type" => "application/json" }
   end
 end
