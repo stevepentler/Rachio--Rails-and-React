8e57952d1b6c69ccdd56efabf485133bbf760c6e
Steve Pentler <stevepentler@gmail.com>
2016-05-10 13:11:40 -0600

    Renders Main and Header, uploads image

---
+++ app/assets/javascripts/components/_header.js.jsx
@@ -0,0 +1,21 @@
+'use strict';
+
+var Header = React.createClass({
+  componentDidMount() {
+      $.ajax({
+      url: '/api/v1/data',
+      type: 'GET',
+      success: (response) => {
+        console.log('it worked', response);
+      }
+    })
+  },
+
+  render() {
+    return (
+      <div>
+        <h1 className="header">Header</h1>
+      </div>
+    )
+  }
+});
