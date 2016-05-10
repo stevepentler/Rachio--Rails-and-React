6ea3fbe50a6c0b41093b5f070b98c9d86504430a
Steve Pentler <stevepentler@gmail.com>
2016-05-09 23:09:24 -0600

    Adds model tests for zone and device, saving to database from service

--- test/models/zone_test.rb
+++ test/models/zone_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ZoneTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'retrieve zones saves device to database' do
+    VCR.use_cassette('device_zones') do
+      service = PersonalInfoService.new
+      assert_difference 'Zone.count', 8 do
+        service.retrieve_device_zones
+      end
+      assert_equal "4f1b562e-1dc3-40d7-acf1-2a1998a47786", Zone.first.zone_id
+      assert_equal "61197df9-d256-4959-8117-7625f919b2b3", Zone.last.zone_id
+    end
+  end
 end
