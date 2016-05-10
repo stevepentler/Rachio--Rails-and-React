44776b6ac7c1e0305ccb290a4e04d818fe5d103a
Steve Pentler <stevepentler@gmail.com>
2016-05-09 18:08:14 -0600

    Cleans up tests

--- test/services/personal_info_service_test.rb
+++ test/services/personal_info_service_test.rb
@@ -10,11 +10,12 @@ class PersonalInfoServiceTest < ActiveSupport::TestCase
     service = PersonalInfoService.new
     assert_equal ENV['USER_ID'], service.retrieve_user_info["id"]
     assert_equal "rachiobeta", service.retrieve_user_info["username"]
+    assert_equal "beta@rach.io", service.retrieve_user_info["email"]
   end
 
-  test 'retrieves device id' do
-    device_id = "c761bfa0-4c49-4b4f-8a79-04e42bea881a"
+  test 'retrieves user device id' do
     service = PersonalInfoService.new
+    device_id = "c761bfa0-4c49-4b4f-8a79-04e42bea881a"
 
     assert_equal device_id, service.retrieve_device_id
   end
