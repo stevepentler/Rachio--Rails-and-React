96f6b2d928e27e659cae869830cdd08549242235
Steve Pentler <stevepentler@gmail.com>
2016-05-11 13:50:44 -0600

    formats widths

--- app/assets/javascripts/components/_zone.js.jsx
+++ app/assets/javascripts/components/_zone.js.jsx
@@ -8,13 +8,13 @@ var Zone = React.createClass({
     return(
       <div className="card zone-card">
         <div className="row">
-          <div className="col s3">
+          <div className="col s5 offset-s1">
             <h4>{zone.name}</h4>
             <h6>Previous Watering:</h6>
             <h6>{zone.lastWateredDuration} mins on {date}</h6>
           </div>
 
-          <div className="col s2">
+          <div className="col s3">
             < WateringForm zone={this.props.zone} waterZone={this.props.waterZone} />
           </div>
         </div>
