5abfbf64c840d960f91476597cf43e81a3333556
Steve Pentler <stevepentler@gmail.com>
2016-05-10 21:00:37 -0600

    Passes props to zones

--- app/assets/javascripts/components/_device.js.jsx
+++ app/assets/javascripts/components/_device.js.jsx
@@ -2,9 +2,11 @@
 
 var Device = React.createClass({
   render() {
+    var device_id = this.props.device.device_id;
     return(
       <div>
-        <h5>Device ID: {this.props.device.device_id}</h5>
+        <h5>Device ID: {device_id}</h5>
+        < Zone device={this.props.device} zones={this.props.zones} />
       </div>
     )
   }
