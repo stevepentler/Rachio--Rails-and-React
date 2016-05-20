8a1710ff8eadcc8e44487dcbac6ed89f2c13cf45
Steve Pentler <stevepentler@gmail.com>
2016-05-20 17:37:07 -0600

    Displays minutes from previousZoneDuration

--- app/assets/javascripts/components/_zone.js.jsx
+++ app/assets/javascripts/components/_zone.js.jsx
@@ -5,13 +5,14 @@ var Zone = React.createClass({
   render() {
     var zone = this.props.zone
     var date = new Date(zone.lastWateredDate).toLocaleString();
+    var previousDuration = parseInt(zone.lastWateredDuration / 60)
     return(
       <div className="card zone-card">
         <div className="row">
           <div className="col s5 offset-s1">
             <h4>{zone.name}</h4>
             <h6>Previous Watering:</h6>
-            <h6>{zone.lastWateredDuration} mins on {date}</h6>
+            <h6>{previousDuration} mins on {date}</h6>
           </div>
 
           <div className="col s4 offset-s1">
